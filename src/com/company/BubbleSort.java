7e530de2c8da6e616655280998761e6d00e07527
Matheus Lima Ferreira <matheus.ferreira.001@acad.pucrs.br>
2018-07-12 10:56:00 -0300

    first commit

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Random;
+
+class BubbleSort
+{
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        int start = (int) System.currentTimeMillis();
+        BubbleSort ob = new BubbleSort();
+
+        Random n = new Random();
+
+        int arr[] =  new int[1000000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = n.nextInt(1000);
+        }
+
+        System.out.println(" array");
+        //ob.printArray(arr);
+
+        ob.bubbleSort(arr);
+
+        System.out.println("Sorted array");
+        //ob.printArray(arr);
+        int end  =  (int) System.currentTimeMillis();
+
+        int atual = end - start;
+        System.out.println("Tempo em segundos: "+ atual/1000);
+    }
+}
---
+++ src/com/company/MergeSort.java
@@ -0,0 +1,130 @@
+package com.company;
+
+import java.util.Random;
+
+/* Java program for Merge Sort */
+class MergeSort
+{
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] >= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            // Find the middle point
+            int m = (l+r)/2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr , m+1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[])
+    {
+        int start = (int) System.currentTimeMillis();
+        Random n = new Random();
+        int arr[] =  new int[199999999];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = n.nextInt(1000);
+        }
+
+        calcula(arr, 10);
+
+        int end  =  (int) System.currentTimeMillis();
+
+        int atual = end - start;
+        System.out.println("Tempo em segundos: "+ atual);
+    }
+
+    static int calcula(int arr[], int quantidade){
+        int total = 0;
+        int j=1;
+        System.out.println("Given Array");
+        //printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        //ob.sort(arr, 0, arr.length-1);
+
+        for (int i = 0; i < arr.length ; i++) {
+            
+        }
+
+        System.out.println("\nSorted array by descending");
+        //printArray(arr);
+
+        return total;
+    }
+}
